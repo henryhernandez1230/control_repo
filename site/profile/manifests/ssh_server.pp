407a0c24685469433d9984a6ff82ecd0483bd6bf
henryhernandez1230 <57072466+henryhernandez1230@users.noreply.github.com>
2020-05-04 12:25:19 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC7eOFn+WWk5YWMCmNS/CGcd8A4+c6472waRcd8OuLP/DEz3Lt/A6L2f96GeONcGOWg7LGVvFrkyxfBvwcgvA5RbMdqX+/Lsi5LSYSQsI7qsS8Zt2XEdXvrIg0gcaifqvvDI6OTeZMJJ1RbgX1bRlx3V8V7m/pFwogdTfbPsuZyfbUnLFCHxY80Hj5bUW+Mx7s0KyeGWjW1gZ2cXrGs8ycpW9OsoH2CrSHYWGNUzvrnm4NC/qadIbzCr1aSDvDEaYBle+skK6zWNOxl2j6klLJoj2ZO1Lld1cWCtA+x3SocPjN/f3MizANPzyYyW6wtdxZyvwNNsGgKMKBYeq05mZkr',
 	}  
 }
